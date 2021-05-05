4ba78eb1fe5a317d5aa9343a8d2ffef04047158d
trynaeat <jamesparks10@gmail.com>
2021-05-04 21:38:13 -0400

    Adding embedded EEPROM-only version of ae2 energy readout

---
+++ ae2/show_energy_embedded.lua
@@ -0,0 +1,22 @@
+local gpu = component.proxy(component.list("gpu")())
+local ae2 = component.proxy(component.list("me_interface")())
+
+local x, y = gpu.getViewport()
+local x_middle = x / 2
+local y_middle = y / 2
+
+computer.beep(500, 0.5)
+
+local function draw_screen()
+	local power = ae2.getAvgPowerUsage() * 2
+	local text = string.format("Avg Usage: %.1f RF/T", power)
+	gpu.setBackground(0x000000)
+	gpu.setForeground(0xFFFFFF)
+	gpu.fill(1, 1, x, y, " ")
+	gpu.set(x_middle - #text / 2, y_middle, text)
+end
+
+while true do
+	draw_screen()
+	computer.pullSignal(0.2)
+end
